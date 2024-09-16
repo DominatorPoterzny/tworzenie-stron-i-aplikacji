bd82235a017ae93dc1e13e31dfdaab335e327759
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2024-09-16 11:09:01 +0200

    zadanie

--- składnia/paragraph.php
+++ składnia/paragraph.php
@@ -11,5 +11,8 @@
     $b = 3.14;
     $c = 'PHP'
      ?>
+     <h3 style="color: red"><?= $a?> jest liczbą pierwszą</h3>
+     <p style="color: #336ee1">Liczba Pi zaokrąglona do dwóch miejsc po przecinku: <?= $b ?></p>
+     <div style="color: green; text-decoration: overline;"><u> <?= $c ?> to język skryptowy wykonany po stronie serwera</u></div>
 </body>
 </html>
