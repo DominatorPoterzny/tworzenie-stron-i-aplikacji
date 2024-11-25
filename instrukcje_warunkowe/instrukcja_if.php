33b9cf414516cd295273619e63f95c3ecfbc31e6
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2024-11-25 11:12:09 +0100

    dupa

--- instrukcje_warunkowe/instrukcja_if.php
+++ instrukcje_warunkowe/instrukcja_if.php
@@ -8,8 +8,11 @@
 <body>
     <?php $losowa=rand(1,30)?>
     <h2><?= $losowa?></h2>
-    <?php if ($losowa>=20){?>
+    <?php if ($losowa>=20){?>;
         <h4>Wylosowana liczba jest większa lub równa 20</h4>
-    <?php }
-</body>?>
+    <?php };
+    if($losowa%2==0){?>;
+        <h4>Parzyste</h4>
+        <?php}?>
+</body>
 </html>
