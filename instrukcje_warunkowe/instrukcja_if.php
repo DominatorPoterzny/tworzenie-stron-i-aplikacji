e63856ac4ea535e6240648b6f1a9569643f5bcee
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2024-11-28 21:58:47 +0100

    zadanie zrobione z IF

--- instrukcje_warunkowe/instrukcja_if.php
+++ instrukcje_warunkowe/instrukcja_if.php
@@ -1,18 +1,49 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    #wylosowane {
+        text-align: center;
+        color: darkorchid;
+    }
+
+    .wieksze {
+        text-align: center;
+        color: magenta;
+    }
+
+    .parzysta {
+        text-align: center;
+        color: navy;
+    }
+    .dziesiec{
+        text-align: center;
+        color: deeppink;
+    }
+</style>
+
 <body>
-    <?php $losowa=rand(1,30)?>
-    <h2><?= $losowa?></h2>
-    <?php if ($losowa>=20){?>;
-        <h4>Wylosowana liczba jest większa lub równa 20</h4>
-    <?php };
-    if($losowa%2==0){?>;
-        <h4>Parzyste</h4>
-        <?php}?>
+    <?php $losowa = rand(1, 30) ?>
+    <h2 id="wylosowane"><?= $losowa ?></h2>
+    <?php if ($losowa >= 20) { ?>
+        <h4 class="wieksze">Wylosowana liczba jest większa lub równa 20</h4>
+    <?php } if ($losowa % 2 == 0) { ?>
+        <h4 class="parzysta">Liczba jest parzysta</h4>
+    <?php } else { ?>
+        <h4 class="parzysta">Liczba nie jest parzysta</h4>
+    <?php } if ($losowa < 10) { ?>
+        <h4 class="dziesiec">Pierwsza dziesiątka</h4>
+    <?php } elseif ($losowa > 21) { ?>
+        <h4 class="dziesiec">Trzecia dziesiątka</h4>
+    <?php } else { ?>
+        <h4 class="dziesiec">Druga dziesiątka</h4>
+    <?php } ?>
+
 </body>
+
 </html>
--- składnia/cudzyslow.php
+++ składnia/cudzyslow.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     $poczatek = 'Ala ma';
@@ -15,4 +17,5 @@
     <?= '$poczatek $liczba $koniec a chce mieć jeszcze dwa.' ?>
 
 </body>
+
 </html>
