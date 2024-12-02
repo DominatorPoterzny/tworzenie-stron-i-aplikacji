f9f6efb2dbe02b74eac032142ef5db6a048c1060
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2024-12-02 09:35:27 +0100

    zadanie

--- instrukcje_warunkowe/od_-10_do_10.php
+++ instrukcje_warunkowe/od_-10_do_10.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    p {
+        color: yellowgreen;
+        border-top: double 10px greenyellow;
+        border-bottom: double 10px greenyellow;
+        font-size: 24px;
+        padding: 10px 0px 10px 0px;
+        text-shadow: 1px 1px 5px forestgreen;
+
+    }
+</style>
+
+
 <body>
-    <?php for ($i = -10; $i <= 10; $i++) {
-        echo $i . ", ";
-    } ?>
+    <p><?php for ($i = -10; $i <= 10; $i++) {
+            if ($i >= 10) {
+                echo "$i.";
+            } else {
+                echo "$i, ";
+            }
+        } ?></p>
 </body>
+
 </html>
