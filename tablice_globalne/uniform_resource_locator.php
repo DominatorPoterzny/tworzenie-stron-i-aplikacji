3177eb2df2ca67602da18202cbcc37563a60345c
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2024-12-16 09:52:46 +0100

    zadanie

---
+++ tablice_globalne/uniform_resource_locator.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="uniform_resource_locator.php" page>Odnośnik + parametry</a>
+</body>
+</html>
--- tablice_petle2/asocjacyjne.php
+++ tablice_petle2/asocjacyjne.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    th,
+    td {
+        border: 1px black dotted;
+        padding: 3px;
+    }
+    table{
+        border-collapse: collapse;
+    }
+</style>
+
 <body>
     <?php $movies = array(
         "Skazani na Shawshank" => "dramat",
@@ -18,9 +30,62 @@
         "Avengers: Wojna bez granic" => "akcja",
         "Dobry, zły i brzydki" => "western"
     ); ?>
-    <?php asort($movies);
-    foreach($movies as $x);{
-        
-    } ?>
+    <table>
+        <caption>sortowanie rosnąco według warotości asort</caption>
+        <th>klucz</th>
+        <th>wartosc</th>
+        <?php asort($movies);
+        foreach ($movies as $x => $y) { ?>
+            <tr>
+                <td> <?= $x ?> </td>
+                <td> <?= $y ?> </td>
+            </tr>
+
+        <?php } ?>
+    </table>
+
+    <table>
+        <caption>sortowanie malejąco według warotości arsort</caption>
+        <th>klucz</th>
+        <th>wartosc</th>
+        <?php arsort($movies);
+        foreach ($movies as $x => $y) { ?>
+            <tr>
+                <td> <?= $x ?> </td>
+                <td> <?= $y ?> </td>
+            </tr>
+
+        <?php } ?>
+    </table>
+
+    <table>
+        <caption>sortowanie rosnąco według kluczy ksort</caption>
+        <th>klucz</th>
+        <th>wartosc</th>
+        <?php ksort($movies);
+        foreach ($movies as $x => $y) { ?>
+            <tr>
+                <td> <?= $x ?> </td>
+                <td> <?= $y ?> </td>
+            </tr>
+
+        <?php } ?>
+    </table>
+
+    <table>
+        <caption>sortowanie rosnąco według kluczy krsort</caption>
+        <th>klucz</th>
+        <th>wartosc</th>
+        <?php krsort($movies);
+        foreach ($movies as $x => $y) { ?>
+            <tr>
+                <td> <?= $x ?> </td>
+                <td> <?= $y ?> </td>
+            </tr>
+
+        <?php } ?>
+    </table>
+
 </body>
+
 </html>
