f27b617bdb6e5b498d2fc944991ddab903ac6c90
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2025-02-03 11:13:55 +0100

    zadanie

--- formularze/litery_w_polach_checkbox.php
+++ formularze/litery_w_polach_checkbox.php
@@ -6,6 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    label{
+        padding-right: 15px;
+    }
+    fieldset{
+        border-color: blue;
+        border-width: 2px;
+       
+    }
+</style>
 
 <body>
 
@@ -13,17 +23,24 @@
     <fieldset>
         <legend>Alfabet</legend>
 
-        <form action="">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
             <?php foreach ($litery as $r) { ?>
 
-                <input type="checkbox" name="<?= $r ?> []" id="<?= $r ?>">
+                <input type="checkbox" name="<?= $r ?>[]" id="<?= $r ?>">
                 <label for="<?= $r ?>"> <?= $r ?> </label>
-
-
 
             <?php } ?>
             <br>
-            <input type="submit" value="Wyślij">
+            <input type="submit" value="Wybierz">
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                foreach ($litery as $r) {
+                    if (isset($_POST[$r])) {
+                        echo 
+                    }
+                }
+            } 
+            ?>
         </form>
     </fieldset>
 </body>
