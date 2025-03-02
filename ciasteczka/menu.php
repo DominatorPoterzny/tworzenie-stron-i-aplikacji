53af8ef5e5ceb06264a56bb1ad2eab9bdfbf497f
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2025-03-02 17:31:20 +0100

    poprawki z menu

--- ciasteczka/menu.php
+++ ciasteczka/menu.php
@@ -1,13 +1,7 @@
 <div id="menu">
-    <a href="/ciasteczka/strona_glowna.php" <?php if(basename($_SERVER["PHP_SELF"]) == "strona_glowna.php" ){
-        $class = 'active'; 
-    } ?> class="<?= $class ?>">Strona główna</a>
-    <a href="/ciasteczka/strona_logowania.php" <?php if(basename($_SERVER["PHP_SELF"]) == "strona_logowania.php" ){
-        $class = 'active'; 
-    } ?> class="<?= $class ?>">Strona logowania</a>
-    
-    <a href="/ciasteczka/ustaw_ciastko.php" <?php if(basename($_SERVER["PHP_SELF"]) == "ustaw_ciastko.php" ){
-        $class = 'active'; 
-    } ?> class="<?= $class ?>">Ustaw ciastko</a>
+    <a href="/ciasteczka/strona_glowna.php" class=" menu  <?php (basename($_SERVER["PHP_SELF"]) == "strona_glowna.php") ?  $active = ', active' : $active = ' ' ?> <?= $active ?>">Strona główna</a>
+    <a href="/ciasteczka/strona_logowania.php" class=" menu  <?php (basename($_SERVER["PHP_SELF"]) == "strona_logowania.php") ? $active = ', active' : $active = ' '  ?> <?= $active ?>">Strona logowania</a>
+
+    <a href="/ciasteczka/ustaw_ciastko.php" class=" menu  <?php (basename($_SERVER["PHP_SELF"]) == "ustaw_ciastko.php") ? $active = ', active' : $active = ' '  ?> <?= $active ?>">Ustaw ciastko</a>
 </div>
 <br>
--- sesja/menu.php
+++ sesja/menu.php
@@ -1,9 +1,7 @@
 <div id="menu">
-    <a href="/sesja/strona_glowna.php" <?php if(basename($_SERVER["PHP_SELF"]) == "strona_glowna.php" ){
-        $class = 'active'; 
-    } ?> >Strona główna</a>
-    <a href="/sesja/strona_logowania.php">Strona logowania</a>
-    <a href="/sesja/strona_tylko_dla_zalogowanych.php">Strona tylko dla zalogowanych</a>
-    <a href="/sesja/strona_wylogowania.php">Strona wylogowania</a>
+    <a href="/sesja/strona_glowna.php" class=" menu  <?php (basename($_SERVER["PHP_SELF"]) == "strona_glowna.php") ?  $active = ', active' : $active = ' ' ?> <?= $active ?>">Strona główna</a>
+    <a href="/sesja/strona_logowania.php" class=" menu  <?php (basename($_SERVER["PHP_SELF"]) == "strona_logowania.php") ?  $active = ', active' : $active = ' ' ?> <?= $active ?>">Strona logowania</a>
+    <a href="/sesja/strona_tylko_dla_zalogowanych.php" class=" menu  <?php (basename($_SERVER["PHP_SELF"]) == "strona_tylko_dla_zalogowanych.php") ?  $active = ', active' : $active = ' ' ?> <?= $active ?>">Strona tylko dla zalogowanych</a>
+    <a href="/sesja/strona_wylogowania.php" class=" menu  <?php (basename($_SERVER["PHP_SELF"]) == "strona_wylogowania.php") ?  $active = ', active' : $active = ' ' ?> <?= $active ?>">Strona wylogowania</a>
 </div>
 <br>
