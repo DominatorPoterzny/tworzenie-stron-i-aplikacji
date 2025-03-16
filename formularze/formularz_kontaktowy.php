d842fc34b5b479b72d4b081379fa2328c153de6a
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2025-03-16 13:38:14 +0100

    zadanie, ale polskie znaki nie działają i nie liczy dobrze wyrazów w imieniu i nazwisku

--- formularze/formularz_kontaktowy.php
+++ formularze/formularz_kontaktowy.php
@@ -15,34 +15,62 @@
         background-color: lightblue;
 
     }
+    span {
+        color: red;
+        font-size: 14px;
+    }
 </style>
 
 <body> <?php
-        $error = [];
+        
+        $error = [
+            'name' => '',
+            'email' => '',
+            'old' => '',
+            'text' => '',
+            'zgoda' => ''
+        ];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (!empty($_POST['name'])) {
-                if (str_word_count(trim($_POST['name'])) == 2) {
-                    $error['dupa'] = '';
-                    echo "cos";
+                if (str_word_count(trim($_POST['name'])) === 2) {
+                    $error['name'] = '';
                 } else {
-                    $error['dupa'] = "Imię i nazwisko musi składać się z dwóch wyrazów";
+                    $error['name'] = "Imię i nazwisko musi składać się z dwóch wyrazów";
                 }
             }
-            if(!empty($_POST['email'])){
-                if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-                    $error['cos'] = '';
-                }else{
-                    $error['cos'] = "Niepoprawny adres email";
+            if (!empty($_POST['email'])) {
+                if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    $error['email'] = '';
+                } else {
+                    $error['email'] = "Niepoprawny adres email";
                 }
+            }
+            if (empty($_POST['old'])) {
+                $error['old'] = "Wybierz przedział wiekowy";
+            } else {
+                $error['old'] = '';
+            }
+            if (strlen($_POST['text']) < 10) {
+                $error['text'] = "Wiadomość musi zawierać conajmniej 10 znaków";
+            } else {
+                $error['text'] = '';
+            }
+            if (empty($_POST['zgoda'])) {
+                $error['zgoda'] = "Potwierdź swoją zgodę";
+            } else {
+                $error['zgoda'] = '';
             }
         } ?>
     <form action="" method="post">
         <label for="name">Imię i Nazwisko<sup>*</sup></label><br>
         <input type="text" name="name" id="name">
-        <?= $error['dupa'] ?>
+        <br>
+        <span><?= $error['name'] ?></span>
         <br>
         <label for="email">Adres Email<sup>*</sup></label><br>
-        <input type="email" name="email" id="email">
+        <input type="text" name="email" id="email">
+        <br>
+        <span><?= $error['email'] ?></span>
         <br>
         <label for="old">Przedział wiekowy<sup>*</sup></label><br>
         <select name="old" id="old">
@@ -53,14 +81,32 @@
             <option value="75">powyżej 75</option>
         </select>
         <br>
+        <span><?= $error['old'] ?></span>
+        <br>
         <label for="text">Wiadomość<sup>*</sup></label><br>
         <textarea name="text" id="text"></textarea>
         <br>
+        <span><?= $error['text'] ?></span>
+        <br>
         <input type="checkbox" name="zgoda" id="zgoda">
         <label for="zgoda">Zgoda na przetwarzanie danych<sup>*</sup></label>
+        <br>
+        <span><?= $error['zgoda'] ?></span>
         <br><br>
         <input type="submit" value="Wyślij">
     </form>
-</body>
+    <pre><?php echo (date("Y-m-d G:i:s")); echo "\n"; print_r($_POST) ?>
+    </pre>
 
+    <?php 
+    if(empty($error['name']) && empty($error['email']) && empty($error['old']) && empty($error['text']) && empty($error['zgoda'])){
+        $file = fopen('formularz_kontaktowy.txt', 'a');
+        // $file = $tab . "\n";
+        // $tab = date("Y-m-d G:i:s") ."\n" . $_POST['name'] . "\n" . $_POST['email'] . "\n" . $_POST['old'] . "\n" . $_POST['text'] . "\n" . $_POST['zgoda'] . "\n";
+        $tab = date("Y-m-d G:i:s") . "\n" . print_r($_POST, true);
+        fwrite($file,$tab);
+        fclose($file);
+    }
+    ?>
+</body>
 </html>
