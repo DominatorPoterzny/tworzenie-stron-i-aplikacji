cbf78dc2e81b0ff819ebc883fac95be1b07bd8a1
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2025-04-28 10:00:13 +0200

    zadanie

---
+++ projekt_biblio/podstrona/dbconn.php
@@ -0,0 +1,1 @@
+<?php $con = mysqli_connect("localhost", "kolomanski", "12345678", "kolomanski") ?>
--- projekt_biblio/projekt.php
+++ projekt_biblio/projekt.php
@@ -52,23 +52,28 @@
         a {
             text-decoration: none;
         }
+
+        .active {
+            background-color: lightblue;
+        }
     </style>
 </head>
+<?php include 'podstrona/dbconn.php' ?>
 
 <body>
     <div class="row">
         <div class="column left" style="background-color:green;">
             <h2>Column 1</h2>
-            
+
             <div class="contener1">
-                <div class="link"> <a href="?podstrona=1">link 1</a> </div>
-                <div class="link"> <a href="?podstrona=2">link 1</a> </div>
-                <div class="link"> <a href="?podstrona=3">link 1</a> </div>
-                <div class="link"> <a href="?podstrona=4">link 1</a> </div>
-                <div class="link"> <a href="?podstrona=5">link 1</a> </div>
-                <div class="link"> <a href="?podstrona=6">link 1</a> </div>
-                <div class="link"> <a href="?podstrona=7">link 1</a> </div>
-                <div class="link"> <a href="?podstrona=8">link 1</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == 'glowny' ? 'active' : '' ?>" href="?podstrona=glowny">Strona główna</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == 'czytelnicy' ? 'active' : '' ?>" href="?podstrona=czytelnicy">Tabela czytelnicy</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == 'dzialy' ? 'active' : '' ?>" href="?podstrona=dzialy">Tabela dzialy</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == 'ksiazki' ? 'active' : '' ?>" href="?podstrona=ksiazki">Tabela ksiazki</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == 'pracownicy' ? 'active' : '' ?>" href="?podstrona=pracownicy">Tabela pracownicy</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == 'stanowiska' ? 'active' : '' ?>" href="?podstrona=stanowiska">Tabela stanowiska</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == 'wypozyczenia' ? 'active' : '' ?>" href="?podstrona=wypozyczenia">Tabela wypozyczenia</a> </div>
+                <div class="link"> <a class="<?= ($_GET['podstrona'] ?? '') == '8' ? 'active' : '' ?>" href="?podstrona=8">link 1</a> </div>
             </div>
         </div>
         <div class="column right" style="background-color:lightgreen;">
@@ -81,11 +86,12 @@
                     echo '<h1>Nie znaleziono żądanej strony</h1>';
                 }
             } else {
-                include 'podstrona/glowna.php';
+                include 'podstrona/glowny.php';
             }
             ?>
         </div>
     </div>
 </body>
+<?php include 'podstrona/dbclose.php' ?>
 
 </html>
