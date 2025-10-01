d96d00a8fea67df9701221d2016736b6214dfccf
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2025-10-01 11:13:27 +0200

    cos

---
+++ JS/7_lekcja/lampa.js
@@ -0,0 +1,9 @@
+const zarowka = document.querySelector('img')
+const radio1 = document.querySelector('#wlacz')
+const radio2 = document.querySelector('#wylacz')
+
+
+radio1.addEventListener('change', log)
+function log(a){
+    
+}
