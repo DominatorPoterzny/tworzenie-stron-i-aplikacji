b411320e99edabd0eb0b897f6a06ce6592bfb447
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2025-10-27 22:19:37 +0100

    zadanibe

--- JS/11_lekcja/test_gracza/test_gracza.js
+++ JS/11_lekcja/test_gracza/test_gracza.js
@@ -1,40 +1,40 @@
-let pod1 = document.querySelector('#pod1')
-let ukr1 = document.querySelector('#ukryte1')
-let ukrtekst1 = ukr1.innerHTML
-let opd1 = document.querySelector('#odp1')
 
-let pod2 = document.querySelector('#pod2')
-let ukr2 = document.querySelector('#ukryte2')
-let ukrtekst1 = ukr1.innerHTML
-let opd2 = document.querySelector('#odp2')
 
-let pod3 = document.querySelector('#pod3')
-let ukr3 = document.querySelector('#ukryte3')
-let ukrtekst1 = ukr1.innerHTML
-let opd3 = document.querySelector('#odp3')
+let odp = document.querySelectorAll('.odp1')
+let ukr = ['lag','aimbot','loot','noob','fanboy','hitbox','camping','respawn']
+let pod = document.querySelectorAll('.pod')
+let pod1 = document.querySelectorAll('.pod1')
+let span = document.querySelectorAll('.ukryte')
+console.log(ukr)
+console.log(odp)
+console.log(pod)
 
-let pod4 = document.querySelector('#pod4')
-let ukr4 = document.querySelector('#ukryte4')
-let ukrtekst1 = ukr1.innerHTML
-let opd4 = document.querySelector('#odp4')
+pod.forEach((element, i)=>{
+    element.addEventListener('click', loga)
+    function loga(b){ 
+        element.innerHTML = '&blk34;'
+        pod1[i].innerHTML = `(${ukr[i].length})`
+        pod1[i].innerHTML = 
+    }
 
-let pod5 = document.querySelector('#pod5')
-let ukr5 = document.querySelector('#ukryte5')
-let ukrtekst1 = ukr1.innerHTML
-let opd5 = document.querySelector('#odp5')
+})
 
-let pod6 = document.querySelector('#pod6')
-let ukr6 = document.querySelector('#ukryte6')
-let ukrtekst1 = ukr1.innerHTML
-let opd6 = document.querySelector('#odp6')
 
-let pod7 = document.querySelector('#pod7')
-let ukr7 = document.querySelector('#ukryte7')
-let ukrtekst1 = ukr1.innerHTML
-let opd7 = document.querySelector('#odp7')
 
-let pod8 = document.querySelector('#pod8')
-let ukr8 = document.querySelector('#ukryte8')
-let ukrtekst1 = ukr1.innerHTML
-let opd8 = document.querySelector('#odp8')
 
+
+
+odp.forEach((element, i) => {
+	element.addEventListener('click', log)
+    function log(a){
+        const odpo = prompt(`Podaj odpowiedz na pytanie ${i+1}:`)
+        if(odpo === ukr[i]){
+            alert(`Twoja odpowiedz jest poprawna`)
+            odp[i].style.display = "none"
+            span[i].style.display = 'inline'
+            span[i].style.fontWeight = 'bold'
+        }else{
+            alert(`Twoja odpowiedz jest zła`)   
+        }
+    }
+})
