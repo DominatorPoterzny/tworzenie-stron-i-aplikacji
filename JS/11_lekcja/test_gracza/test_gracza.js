d28273fad81bd8627ed852549cacbfbd8eff46ab
DominatorPoterzny <144838010+DominatorPoterzny@users.noreply.github.com>
2025-10-28 00:01:44 +0100

    dupa

--- JS/11_lekcja/test_gracza/test_gracza.js
+++ JS/11_lekcja/test_gracza/test_gracza.js
@@ -1,7 +1,5 @@
-
-
 let odp = document.querySelectorAll('.odp1')
-let ukr = ['lag','aimbot','loot','noob','fanboy','hitbox','camping','respawn']
+let ukr = ['lag', 'aimbot', 'loot', 'noob', 'fanboy', 'hitbox', 'camping', 'respawn']
 let pod = document.querySelectorAll('.pod')
 let pod1 = document.querySelectorAll('.pod1')
 let span = document.querySelectorAll('.ukryte')
@@ -9,32 +7,34 @@ console.log(ukr)
 console.log(odp)
 console.log(pod)
 
-pod.forEach((element, i)=>{
-    element.addEventListener('click', loga)
-    function loga(b){ 
-        element.innerHTML = '&blk34;'
-        pod1[i].innerHTML = `(${ukr[i].length})`
-        pod1[i].innerHTML = 
-    }
-
-})
-
-
-
-
-
+pod.forEach((element, i) => {
+	element.klik = 1;
+    const gwiazda = '*'.repeat(ukr[i].length)
+	element.addEventListener('click', function () {
+		if (element.klik === 1) {
+			element.innerHTML = '&blk34;';
+			pod1[i].innerHTML = `${gwiazda} (${ukr[i].length}) `;
+			element.klik++;
+		} if (element.klik === 2) {
+			element.innerHTML = '&blk12;';
+			pod1[i].innerHTML = `${gwiazda} (${ukr[i].length}) `;
+			element.klik++;
+		}
+        
+	});
+});
 
 odp.forEach((element, i) => {
 	element.addEventListener('click', log)
-    function log(a){
-        const odpo = prompt(`Podaj odpowiedz na pytanie ${i+1}:`)
-        if(odpo === ukr[i]){
-            alert(`Twoja odpowiedz jest poprawna`)
-            odp[i].style.display = "none"
-            span[i].style.display = 'inline'
-            span[i].style.fontWeight = 'bold'
-        }else{
-            alert(`Twoja odpowiedz jest zła`)   
-        }
-    }
+	function log(a) {
+		const odpo = prompt(`Podaj odpowiedz na pytanie ${i + 1}:`)
+		if (odpo === ukr[i]) {
+			alert(`Twoja odpowiedz jest poprawna`)
+			odp[i].style.display = 'none'
+			span[i].style.display = 'inline'
+			span[i].style.fontWeight = 'bold'
+		} else {
+			alert(`Twoja odpowiedz jest zła`)
+		}
+	}
 })
