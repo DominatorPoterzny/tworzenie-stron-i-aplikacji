d1c75364952c4fb076c939fd24441f77e39b9c4e
DominatorPoterzny <filip.kolomanski2006@wp.pl>
2025-11-23 16:46:22 +0100

    dupa

---
+++ egzaminy/2025_01_03/kalendarz.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalendarz</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Dni, miesiące, lata...</h1>
+    </header>
+    <section>
+        <p></p>
+    </section>
+    <aside class="left">
+        <table>
+            <tr>
+                <td>liczba dni</td>
+                <td>miesiąc</td>
+            </tr>
+            <tr>
+                <td colspan="7">31</td>
+                <td>styczeń</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>marzec</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>maj</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>lipiec</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>sierpień</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>październik</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>grudzień</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>kwiecień</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>czerwiec</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>wrzesień</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>listopad</td>
+            </tr>
+            <tr>
+                <td>28 lub 29</td>
+                <td>luty</td>
+            </tr>
+
+        </table>
+    </aside>
+    <main></main>
+    <aside class="right"></aside>
+    <footer></footer>
+</body>
+
+</html>
