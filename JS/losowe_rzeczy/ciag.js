140199489f17efb835b452e42a4396844f79829b
DominatorPoterzny <filip.kolomanski2006@wp.pl>
2025-11-23 12:41:47 +0100

    DADA

--- JS/losowe_rzeczy/ciag.js
+++ JS/losowe_rzeczy/ciag.js
@@ -1,12 +1,13 @@
 let field = document.querySelector('fieldset')
-let a1 = 5
-let q = 7
-for (let i = 1; i <= 7; i++) {
+let a1 = 1
+let q = 8
+for (let i = 1; i <= 999; i++) {
 	let p1 = document.createElement('p')
 	let x = ', '
     let y = '.'
 	let an = a1 * Math.pow(q, i - 1)
     console.log(i)
+    console.log(an)
 	if (i === 7) {
 		p1.innerHTML = `${an}. `
         field.appendChild(p1)
