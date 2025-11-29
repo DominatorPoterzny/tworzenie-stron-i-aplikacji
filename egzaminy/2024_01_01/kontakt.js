a41da086422488f4b929115edeeb61e17a4bb946
DominatorPoterzny <filip.kolomanski2006@wp.pl>
2025-11-29 15:27:22 +0100

    fyujkghjf

--- egzaminy/2024_01_01/kontakt.js
+++ egzaminy/2024_01_01/kontakt.js
@@ -1,13 +1,12 @@
 let input = document.querySelectorAll('input')
-let wiadomosc = document.querySelector('#zloszenie')
+let wiadomosc = document.querySelector('#zgloszenie')
 let nowa_wiadomosc = document.querySelector('#wiadomosc')
 
-if(input[3].checked == true){
-    
-}
-else{
-    console.log("dupacyce")
-}
 function przeslij() {
-    wiadomosc.innerHTML = `${input[0].value} ${input[1].value} <br>`
+	if (input[3].checked == true) {
+        nowa_wiadomosc.innerHTML = `<span>${input[0].value} ${input[1].value}</span><br> Treść twojej sprawy: ${wiadomosc.value}`
+	} else {
+		nowa_wiadomosc.innerText = `Musisz zapoznać się z regulaminem`
+	}
+	
 }
