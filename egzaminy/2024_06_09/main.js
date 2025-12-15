a6a2ef1c7b064dbb66bfe1ad46cdbc0aa4d36463
DominatorPoterzny <filip.kolomanski2006@wp.pl>
2025-12-15 20:34:15 +0100

    dupa

---
+++ egzaminy/2024_06_09/main.js
@@ -0,0 +1,25 @@
+let aktywnezdjecie = document.querySelector('#Aktywnezdjecie')
+let licznik = 1
+function doprzodu(){
+    console.log(licznik)
+    console.log('dupaprzód')
+    if(licznik !=8){
+        aktywnezdjecie.src = `${licznik}.jpg`
+        licznik ++
+    }else{
+        aktywnezdjecie.src = `1.jpg`
+        licznik = 1
+    }
+}
+function dotylu(){
+    console.log(licznik)
+    console.log('dupatył')
+   if(licznik != 1){
+    licznik --
+    aktywnezdjecie.src = `${licznik}.jpg`
+   
+   }else{
+    aktywnezdjecie.src = '7.jpg'
+    licznik = 7
+   }
+}
